58749fe00503b51e4d33e065b1a3e5a75d3f0319
BeloPetra <belopetra@gmail.com>
2022-05-28 18:16:39 +0200

    new file:   JS/main.js new file:   JS/scope.js 	new file:   bootstrap.html new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.css new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.css.map new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.rtl.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.rtl.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.rtl.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-grid.rtl.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.rtl.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.rtl.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.rtl.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-reboot.rtl.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.rtl.css	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.rtl.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.rtl.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap-utilities.rtl.min.css.map#	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.rtl.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.rtl.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.rtl.min.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/bootstrap.rtl.min.css.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/css/mycss.css 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.js.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.min.js.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.esm.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.esm.js.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.esm.min.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.esm.min.js.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.js.map 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js 	new file:   bootstrap/bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js.map 	new file:   contact.bt.html	new file:   css/img/bgpattern.jpg 	new file:   css/img/cs8a-hero.jpg 	new file:   css/img/pirate-seamless-pattern-background_153454-536.webp 	new file:   css/styles/menu.css 	new file:   css/styles/selectors.css 	new file:   css/styles/style.css 	modified:   main.html 	modified:   pages/Menu.html	modified:   pages/elerhetoseg.html 	modified:   pages/orders.html 	new file:   pages/selectors.html

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+console.log("Hello, this is console message");
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showSumPrice(price, amountNumber);
+    
+}
+
+function showSumPrice(price=1000, amountNumber=1) {
+
+    let showamount = document.querySelector("span.show-amount");
+    
+        if (amountNumber > 10) {
+            alert("Maximum 10 terméket vásárolhat");
+    
+        } else if ( amountNumber < 1 ) {
+            alert("Minimum 1 terméket kell vásárolnia")
+        } else { 
+            let amount = amountNumber * price;
+            showamount.innerHTML = amount;}
+    
+}
+
+
---
+++ JS/scope.js
@@ -0,0 +1,11 @@
+//Global scope.
+const NAME = 'Sanyi';
+
+function test() {
+    const NAME = 'Pisti';
+    console.log(NAME);
+}
+
+test();
+
+console.log(NAME);
